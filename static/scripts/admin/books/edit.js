e0bd760bd8e5744785b6e41d579c5e5e152187fa
Joe Baker <joeblakeb@gmail.com>
2023-05-09 14:02:21 +0100

    Fix deleting book on android

--- static/scripts/admin/books/edit.js
+++ static/scripts/admin/books/edit.js
@@ -1,13 +1,16 @@
 "use strict";
 
+var bookIDToDelete = 0;
+
 /**
  * Ask the user to confirm that they want to delete the book.
  * 
  * @param {bookID} id The ID of the book to delete.
  */
 function deleteBookButton(bookID) {
+    bookIDToDelete = bookID
     androidCompat.showConfirmDialog("Are you sure you want to delete this book?", () => {
-        deleteBook(bookID);
+        deleteBook();
     }, () => {
         androidCompat.showToast("Book was not deleted");
     });
@@ -16,9 +19,9 @@ function deleteBookButton(bookID) {
 /**
  * Delete the the book with the given ID.
  */
-function deleteBook(bookID) {
+function deleteBook() {
     var xhr = new XMLHttpRequest();
-    xhr.open("DELETE", "/admin/books/delete/"+bookID, true);
+    xhr.open("DELETE", "/admin/books/delete/" + bookIDToDelete, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             androidCompat.showToast("Book deleted");
